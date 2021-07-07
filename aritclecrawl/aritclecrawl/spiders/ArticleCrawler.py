a6aecf9720a67de9832e53418c18303beae6f6bd
rajarshi-2000 <51237949+rajarshi-2000@users.noreply.github.com>
2021-07-07 16:12:10 +0530

    Extract Data in json

--- aritclecrawl/aritclecrawl/spiders/ArticleCrawler.py
+++ aritclecrawl/aritclecrawl/spiders/ArticleCrawler.py
@@ -1,10 +1,21 @@
 import scrapy
+import json
+
+
+def readDataFile(path: str) -> list:
+    links = []
+    with open(path, "r") as readFile:
+        while True:
+            line = readFile.readline()
+            if not line:
+                break
+            links.append(line[1:])
+    return links
 
 
 class ArticleSpider(scrapy.Spider):
     name = "articles"
-    start_urls = ["http://www.sciencedirect.com/science/article/pii/S0003687013000549",
-                  "http://www.sciencedirect.com/science/article/pii/S0003687013000550"]
+    start_urls = readDataFile("./data/cspubsum_ids.txt")
 
     def start_requests(self):
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
@@ -15,5 +26,8 @@ class ArticleSpider(scrapy.Spider):
         yield {
             'title': response.css("span.title-text::text").get(),
             'highlights': response.css("div.author-highlights dl.list dd.list-description p::text").getall(),
-            'abstract': response.css("div.author p::text").get()
+            'abstract': response.css("div.author p::text").getall()
         }
+
+
+
