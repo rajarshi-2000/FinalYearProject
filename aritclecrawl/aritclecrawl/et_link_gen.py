0b4c98e2884d3eee9ba36800ed4e28fc39084cd8
rajarshi-2000 <51237949+rajarshi-2000@users.noreply.github.com>
2022-01-22 13:23:10 +0530

    dynamic starttime based on year

--- aritclecrawl/aritclecrawl/et_link_gen.py
+++ aritclecrawl/aritclecrawl/et_link_gen.py
@@ -1,12 +1,14 @@
 # https://economictimes.indiatimes.com/archivelist/year-2018,month-1,starttime-43101.cms
 import os
-
+from datetime import date
 months = [(31, 'Jan'), (28, 'Feb'), (31, 'Mar'), (30, 'Apr'), (31, 'May'), (30, 'Jun'),
           (31, 'Jul'), (31, 'Aug'), (30, 'Sep'), (31, 'Oct'), (30, 'Nov'), (31, 'Dec')]
 
 
 def get_links(year):
-    starttime = 43101
+    d0 = date(2018, 1, 1)
+    d1 = date(year, 1, 1)
+    starttime = 43101 + (d1-d0).days
     folder = f"E:/FinalYearProject/aritclecrawl/{year}/"
     str_domain = "https://economictimes.indiatimes.com/archivelist/"
     str_end = ".cms"
